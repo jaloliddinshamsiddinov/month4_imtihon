b1a5b647b30906d05e6b28d3e7189cbbc1c91ed5
jaloliddinshamsiddinov <125132177+jaloliddinshamsiddinov@users.noreply.github.com>
2023-08-26 10:50:34 +0500

    new repo

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
-let arr = ["I", "study", "JavaScript", "right", "now"]
+/* task _____________1 */
 
+let arr = ["I", "study", "JavaScript", "right", "now"];
+arr.splice(3, 1);
+console.log(arr);
+
+/* task _____________2 */
+let str = "salom dunyo";
+function reverseApp(s) {
+  return s.split("").reverse().join("");
+}
+console.log(reverseApp(str));
+
+/* task _____________3 */
+let str2 = "1java2script3 5 deve6lo4per";
+
+function splitApp(str) {
+  for (let i = 0; i < str.length; i++) {
+    if (typeof(+str[i]) == "Number") {
+      console.log("bor");
+      console.log(str[i]);
+    } else {
+    //   console.log("yo'q");
+        console.log(+str[i]);
+    }
+  }
+}
+
+splitApp(str2);
